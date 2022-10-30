94b84c68edebfcff1d34b5ce12dd07202cacbc99
Yijun Li <Mopriestt@gmail.com>
2022-10-30 13:29:01 +1100

    Get chat api and data class to json.

--- src/main/kotlin/mopriestt/homepage/home_page_server/chatroom/ChatRoomController.kt
+++ src/main/kotlin/mopriestt/homepage/home_page_server/chatroom/ChatRoomController.kt
@@ -1,5 +1,6 @@
 package mopriestt.homepage.home_page_server.chatroom
 
+import com.google.gson.Gson
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController
 @CrossOrigin("*")
 @RequestMapping("/chatroom")
 class ChatRoomController {
+  private val gson = Gson()
   val chats = mutableListOf<String>();
+
   @GetMapping
-  fun getChatHistory(): List<String> {
-    return chats
-  }
+  fun getChatHistory(): String = gson.toJson(ChatroomRepository.fetchChatThreads())
+
   @PostMapping
   fun postMessage(@RequestBody msg: String): List<String> {
     return chats.apply { this += msg }
--- src/main/kotlin/mopriestt/homepage/home_page_server/chatroom/ChatroomRepository.kt
+++ src/main/kotlin/mopriestt/homepage/home_page_server/chatroom/ChatroomRepository.kt
@@ -1,5 +1,10 @@
 package mopriestt.homepage.home_page_server.chatroom
 
-class ChatroomRepository {
+import mopriestt.homepage.home_page_server.database.SqlClient
 
+object ChatroomRepository {
+    fun fetchChatThreads() : List<ChatThreadModel> {
+        val sql = "SELECT * FROM chat_threads"
+        return SqlClient.query(sql)
+    }
 }
